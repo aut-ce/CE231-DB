b0592806b091db16d5ef152c268576ab19f1a31e
Parham Alvani <parham.alvani@gmail.com>
2019-10-12 01:09:34 +0330

    feat: Add Rides and Dicounts Tables

--- HW1/tables.sql
+++ HW1/tables.sql
@@ -11,8 +11,17 @@
 /*
  * Copyright (c) 2019 Parham Alvani.
 */
+
 drop table if exists drivers;
 create table drivers ( id int primary key auto_increment, first_name text not null, last_name text not null, status int default 0, phone text not null);
 
 drop table if exists passengers;
 create table passengers ( id int primary key auto_increment, first_name text not null, last_name text not null, phone text not null);
+
+drop table if exists rides;
+create table rides ( id int primary key auto_increment, passenger int references passengers(id), driver int references drivers(id),
+        source_lat decimal(10, 8) not null, source_lng decimal(11, 8) not null,
+        destination_lat decimal(10, 8) not null, destination_lng decimal(11, 8) not null,
+        second_destination_lat decimal(10, 8), second_destination_lng decimal(11, 8));
+
+drop table if exists discounts;
